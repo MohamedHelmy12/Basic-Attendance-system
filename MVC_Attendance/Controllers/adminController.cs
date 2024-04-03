2491651aa2c432d4e2cc458263cfa4f832cf3a8d
MohamedHelmy12 <kasaby14@gmail.com>
2024-04-03 05:00:32 +0200

    Super work

---
+++ MVC_Attendance/Controllers/adminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Attendance.Controllers
+{
+    public class adminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult std_table()
+        {
+            return View();
+        }
+        public IActionResult ins_table()
+        {
+            return View();
+        }
+        public IActionResult create_std()
+        {
+            return View();
+        }
+    }
+}
