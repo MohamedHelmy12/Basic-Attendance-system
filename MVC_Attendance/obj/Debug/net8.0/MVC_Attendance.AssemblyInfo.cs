7889df28766e187b66d3e66aad3ece3eb4aaff40
ah-Elsharkawy <aelsharkawy56@gmail.com>
2024-04-18 17:07:45 +0200

    fixing permission update

--- MVC_Attendance/obj/Debug/net8.0/MVC_Attendance.AssemblyInfo.cs
+++ MVC_Attendance/obj/Debug/net8.0/MVC_Attendance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC_Attendance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13526f32615f9a3cb62f7ae0132c310de173cc82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24660201c6feb141a5d058a6081efcc07a67ac1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC_Attendance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC_Attendance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
