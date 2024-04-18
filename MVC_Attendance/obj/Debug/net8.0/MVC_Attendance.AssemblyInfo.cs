cf3e934c16c61909ebc94325971fe50847403e1e
MohamedHelmy12 <kasaby14@gmail.com>
2024-04-18 23:15:09 +0200

    merge

--- MVC_Attendance/obj/Debug/net8.0/MVC_Attendance.AssemblyInfo.cs
+++ MVC_Attendance/obj/Debug/net8.0/MVC_Attendance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC_Attendance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7889df28766e187b66d3e66aad3ece3eb4aaff40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5b758156e10c6e654287e16188868f1a4acd248")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC_Attendance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC_Attendance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
